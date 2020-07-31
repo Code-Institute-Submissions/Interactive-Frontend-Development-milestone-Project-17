0ebd02fe756def6035f53b531bf281177344597e
lhynes <louise.m.hynes@gmail.com>
2020-07-31 22:55:52 +0000

    renamed js file

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function myNoticeFunction() {
+  alert("Exciting times ahead ... we will be adding this city soon!");
+}
+
+function myGalwayFunction() {
+     window.location = "galwayCatagoryPage.html";
+  
+}
+
