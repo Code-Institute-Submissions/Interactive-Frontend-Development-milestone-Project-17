f88e04b5a149551b044189706f1b7dca839f8e17
lhynes <louise.m.hynes@gmail.com>
2020-08-04 22:41:08 +0000

    initial commit

---
+++ assets/js/submitBusiness.js
@@ -0,0 +1,18 @@
+//console.log("Hello!");
+
+function sendMail(contactForm) {
+    emailjs.send("gmail","business_submission", {
+        "from_name": contactForm.fullname.value,
+        "from_email": contactForm.emailaddress.value,
+        "business_submission": contactForm.businesssubmissionsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
